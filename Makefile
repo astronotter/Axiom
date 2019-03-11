b8b0b691ee1ccc1329aa58b83c55d584331ac7b5
Andrew Pregent <ottersocks@outlook.com>
2019-03-10 22:47:27 -0400

    Switch to flat-file db to allow sharing.

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 .PHONY: all run
 all: 
 	javac -cp ".:axiom/*:lib/*" axiom/*.java
-	jar cfm Axiom.jar Manifest.txt axiom/*.class META-INF/persistence.xml
+	jar cfm Axiom.jar Manifest.txt axiom/*.class
 run: all
-	java -javaagent:lib/openjpa-3.0.0.jar -jar Axiom.jar
+	java -jar Axiom.jar
--- axiom/Axiom.java
+++ axiom/Axiom.java
@@ -1,41 +1,80 @@
 package axiom;
 
 import java.util.*;
-import javax.persistence.*;
+import java.util.stream.*;
+import java.io.*;
+import java.nio.file.*;
+import java.lang.reflect.*;
 
-@Entity
-class Category {
-   // The id is unique to both Category and Question tables, so that a category
-   // can unambiguously categorize both questions and other categories.
-   @Id
-   @TableGenerator(name="gid", table="gid", initialValue=1)
-   @GeneratedValue(strategy=GenerationType.TABLE, generator="gid")
+
+interface IFlatTable {
+   // Get the name of the corresponding table in the database. Defaults to the
+   // name of the subclass itself (this is a new feature in java 8).
+   default String getTableName() {
+      return this.getClass().getName();
+   }
+}
+
+class FlatDB {
+   private List<Object> entities;
+   
+   public FlatDB(String filename) throws Exception {
+      this.entities = new ArrayList<Object>();
+   
+      String lines[] = Files.lines(Paths.get(filename)).toArray(String[]::new);
+      for (String line : lines) {
+         String parts[] = line.split(":");
+         Class tableClass = Class.forName(parts[0]);
+         if (tableClass == null) {
+            System.err.println(String.format("Warning: Cannot find class '%s'. Ignoring entry.", parts[0]));
+            continue;
+         }
+         Method deserializeMethod = tableClass.getDeclaredMethod("deserialize", String.class);
+         if (deserializeMethod == null) {
+            System.err.println(String.format("Warning: Cannot find class '%s'. Ignoring entry.", parts[0]));
+            continue;
+         }
+         this.entities.add(deserializeMethod.invoke(tableClass, parts[1]));
+      }
+   }
+   
+   public void save() {
+   }
+   
+   // Wierd quirk in the syntax: Even though this generic says extends, it can
+   // be an interface as well.
+   public <T extends IFlatTable> List<T> select() {
+      return null;
+   }
+   public <T extends IFlatTable> void insert(T object) {
+   }
+}
+
+
+class Category implements IFlatTable {
    private long id;
-   @Basic
    private String name;
    
-   public Category(String name) {
+   public Category(long id, String name) {
       this.name = name;
    }
    public String getName() {
       return name;
    }
+   public static Category deserialize(String text) {
+      String parts[] = text.split(",");
+      // TODO validate input
+      return new Category(Integer.parseInt(parts[0]), parts[1]);
+   }
 }
 
-@Entity
-class Question {
-   // The id is unique to both Category and Question tables, so that a category
-   // can unambiguously categorize both questions and other categories.
-   @Id
-   @TableGenerator(name="gid", table="gid", initialValue=1)
-   @GeneratedValue(strategy=GenerationType.TABLE, generator="gid")
+class Question implements IFlatTable {
    private long id;
-   @Basic
    private String text;
-   @Basic
    private String answer;
    
-   public Question(String text, String answer) {
+   public Question(long id, String text, String answer) {
+      this.id = id;
       this.text = text;
       this.answer = answer;
    }
@@ -46,43 +85,33 @@ class Question {
    public String getAnswer() {
       return answer;
    }
+   public static Question deserialize(String text) {
+      String parts[] = text.split(",");
+      // TODO validate input
+      return new Question(Integer.parseInt(parts[0]), parts[1], parts[2]);
+   }
 }
 
 public class Axiom {
-   @SuppressWarnings("unchecked")
-	public static void main(String []args) {
-      EntityManagerFactory factory =
-         Persistence.createEntityManagerFactory(
-            "axiom", System.getProperties());
-      EntityManager em = factory.createEntityManager();
+	public static void main(String []args) throws Exception {
+      FlatDB db = new FlatDB("axiom-db.xml");
       
-      if (args.length == 0)
+      if (args.length == 0) {
+         //for (Question q : db.<Question>select())
+         //   System.out.println(String.format("%s", q.getText()));
          return;
+      }
       switch (args[0]) {
          case "create": {
             if (args.length != 3)
                return;
-            EntityTransaction tr = em.getTransaction();
-            tr.begin();
-            em.persist(new Question(args[1], args[2]));
-            tr.commit();
+            db.insert(new Question(0, args[1], args[2]));
             break;
          }
          default: {
-            EntityTransaction tr = em.getTransaction();
-            tr.begin();
-            List<Question> results = em
-               .createQuery("SELECT q FROM Question q")
-               .getResultList();
-            System.out.println(String.format("questions (%d):", results.size()));
-            for (Question q : results) {
-               System.out.println(String.format("%s:%s", q.getText(), q.getAnswer()));
-            }
-            tr.commit();
             break;
          }
       }
-      em.close();
-      factory.close();
+      db.save();
 	}
 }
